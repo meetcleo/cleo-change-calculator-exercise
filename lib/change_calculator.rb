659113e047b45c2a7ecbe7cb66db5cdc27fde6ae
Murray Steele <muz@h-lame.com>
2021-05-20 09:33:04 +0100

    Set up empty repo for change calculator exercise
    It has an empty ChangeCalculator class in lib and we've set up both rspec
    and minitest to test that class. This should mean candidates only need to
    clone this repo, bundle install, and then choose their preferred testing
    framework to get started.

---
+++ lib/change_calculator.rb
@@ -0,0 +1,2 @@
+class ChangeCalculator
+end
---
+++ spec/change_calculator_spec.rb
@@ -0,0 +1,7 @@
+require 'change_calculator'
+
+RSpec.describe ChangeCalculator do
+  it 'exists' do
+    expect(defined? described_class).to be_truthy
+  end
+end
